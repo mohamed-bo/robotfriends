185a2cfa093852983af3013971aa77f4f9e712c1
Mohamed Boulahia <55628321+mohamed-bo@users.noreply.github.com>
2022-10-25 13:04:14 +0100

    Add files via upload

---
+++ src/links.js
@@ -0,0 +1,74 @@
+export const books=[
+{
+	id:0,
+	name: 'book1'
+	url: 'url1'
+	description : 'des1'
+	category : 'ca1'
+}
+{
+	id:1,
+	name: 'book2'
+	url: 'url2'
+	description : 'des1'
+	category : 'ca1'
+}
+{
+	id:2,
+	name: 'book3'
+	url: 'url3'
+	description : 'des1'
+	category : 'ca1'
+}
+{
+	id:3,
+	name: 'book4'
+	url: 'url4'
+	description : 'des1'
+	category : 'ca1'
+}
+{
+	id:4,
+	name: 'book2'
+	url: 'url2'
+	description : 'des1'
+	category : 'ca1'
+}
+{
+	id:5,
+	name: 'book6'
+	url: 'url6'
+	description : 'des1'
+	category : 'ca1'
+}
+{
+	id:6,
+	name: 'book7'
+	url: 'url7'
+	description : 'des1'
+	category : 'ca1'
+}
+{
+	id:7,
+	name: 'book8'
+	url: 'url8'
+	description : 'des1'
+	category : 'ca1'
+}
+{
+	id:8,
+	name: 'book9'
+	url: 'url9'
+	description : 'des1'
+	category : 'ca1'
+}
+{
+	id:9,
+	name: 'book10'
+	url: 'url10'
+	description : 'des1'
+	category : 'ca1'
+}
+
+
+]
